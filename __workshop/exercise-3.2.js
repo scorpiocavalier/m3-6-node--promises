d1f7c78515f06c92ecddfea7aabb4491ce8eb2fe
Arturo Chen <art.can.fly@gmail.com>
2020-07-10 16:09:02 -0400

    3.2 done

--- __workshop/exercise-3.2.js
+++ __workshop/exercise-3.2.js
@@ -1,18 +1,34 @@
 // Exercise 3.2 - `getAddressPosition`
 // ---------------------------------
 
-const opencage = require('opencage-api-client');
-require('dotenv').config();
+const opencage = require('opencage-api-client')
+require('dotenv').config({ path: '../.env' })
 
 const getPositionFromAddress = (address) => {
   const requestObj = {
-    key: '<MY_API_KEY>',
-    q: '<QUERY_STRING>',
-  };
+    key: process.env.OPENCAGE_API_KEY,
+    q: address,
+  }
 
-  // return something...
-};
+  return (
+    opencage
+      .geocode(requestObj)
+      .then(data => {
+        if (data.status.code == 200 && data.results.length > 0) {
+          const place = data.results[0]
+          return place.geometry
+        }
+      })
+      .catch(err => console.log('error', err.message))
+  )
+}
 
-getPositionFromAddress(
-  '1455 Boulevard de Maisonneuve O, Montréal, QC H3G 1M8'
-).then((response) => console.log(response));
+const addresses = [
+  '1455 Boulevard de Maisonneuve O, Montréal, QC H3G 1M8',
+  '5455 Grande-Allee Brossard, QC J4Z3H1',
+  '2035A Rue Victoria, Brossard, QC J4S 1H1'
+]
+
+addresses.forEach(address => {
+  getPositionFromAddress(address).then(res => console.log(res))
+})
