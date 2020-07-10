e8fb303b584d0325b606b6cdac445159de485384
Arturo Chen <art.can.fly@gmail.com>
2020-07-10 12:11:03 -0400

    E3.1 done

--- __workshop/exercise-3.1.js
+++ __workshop/exercise-3.1.js
@@ -2,8 +2,19 @@
 // -----------------------------
 
 // require the 'request-promise' module.
+const request = require('request-promise')
 
 // Returns the current position of the ISS
 const getIssPosition = () => {
-  // write some code...
-};
+
+  const baseUrl = 'http://api.open-notify.org/iss-now.json'
+  
+  return (
+    request(baseUrl)
+      .then(res => JSON.parse(res))
+      .then(parsedRes => ({ latitude:lat, longitude:lng } = parsedRes.iss_position))
+      .catch(err => console.log('Error:', err))
+  )
+}
+
+getIssPosition().then((lat, lng) => console.log(lat, lng))
