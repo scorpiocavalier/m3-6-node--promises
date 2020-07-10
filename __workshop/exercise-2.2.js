5eaa2c770543f18852516df8c0ca93cf80fab315
Arturo Chen <art.can.fly@gmail.com>
2020-07-10 11:53:07 -0400

    E2.3 done

--- __workshop/exercise-2.2.js
+++ __workshop/exercise-2.2.js
@@ -6,12 +6,15 @@ const request = require('request-promise')
 const baseURL = 'https://journeyedu.herokuapp.com'
 const endpoint = '/hello/'
 
-const greeting = (langCode) => {
+const greeting = langCode => {
   return request(baseURL + endpoint + langCode)
     .then(res => JSON.parse(res))
     .then(parsedRes => parsedRes.data)
-    .catch((err) => console.log('Error: ', err))
+    .catch(err => console.log('Error: ', err))
 }
 
 // Testing
 greeting('fr').then((result) => console.log(result))
+greeting('en').then((result) => console.log(result))
+greeting('es').then((result) => console.log(result))
+greeting('it').then((result) => console.log(result))
--- __workshop/exercise-2.3.js
+++ __workshop/exercise-2.3.js
@@ -2,6 +2,20 @@
 // ----------------------------
 
 // require the 'request-promise' module.
-const request = require('request-promise');
+const request = require('request-promise')
+const baseURL = 'https://journeyedu.herokuapp.com'
+const endpoint = '/hello/'
 
-// get the code you wrote in 2.2 and paste it here...
+const greeting = langCode => {
+  return request(baseURL + endpoint + langCode)
+    .then(res => JSON.parse(res))
+    .then(parsedRes => parsedRes.data)
+    .catch(err => err.error ? JSON.parse(err.error) : err)
+}
+
+// Testing
+greeting('fr').then((result) => console.log(result))
+greeting('en').then((result) => console.log(result))
+greeting('es').then((result) => console.log(result))
+greeting('it').then((result) => console.log(result))
+greeting('cb').then((result) => console.log(result))
