0160481a82bc1d945d332ad8d237015e22b5b11e
Arturo Chen <art.can.fly@gmail.com>
2020-07-10 10:42:28 -0400

    E1 done

--- __workshop/exercise-1.js
+++ __workshop/exercise-1.js
@@ -1,23 +1,38 @@
 // Exercise 1
 // ------------
-const arrayOfWords = ['cucumber', 'tomatos', 'avocado'];
-const complicatedArray = ['cucumber', 44, true];
+const arrayOfWords = ['cucumber', 'tomatos', 'avocado']
+const complicatedArray = ['cucumber', 44, true]
+const error = 'Not every element in this array is a string.'
 
-const makeAllCaps = (array) => {
-  // write some code
-};
+const makeAllCaps = array => {
+  return (
+    new Promise((resolve, reject) => {
+      if(array.every(word => typeof word === 'string'))
+        resolve(array.map(word => word.toUpperCase()))
+      else
+        reject(error)
+    })
+  )
+}
 
-const sortWords = (array) => {
-  // write some code
-};
+const sortWords = array => {
+  return (
+    new Promise((resolve, reject) => {
+      if(array.every(word => typeof word === 'string'))
+        resolve(array.sort())
+      else
+        reject(error)
+    })
+  )
+}
 
 // Calling (testing)
 makeAllCaps(arrayOfWords)
   .then(sortWords)
-  .then((result) => console.log(result))
-  .catch((error) => console.log(error));
+  .then(result => console.log(result))
+  .catch(error => console.log(error))
 
 makeAllCaps(complicatedArray)
   .then(sortWords)
-  .then((result) => console.log(result))
-  .catch((error) => console.log(error));
+  .then(result => console.log(result))
+  .catch(error => console.log(error))
