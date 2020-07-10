1fecfae89dfd6ad076594039e242902b9b3f754e
Arturo Chen <art.can.fly@gmail.com>
2020-07-10 10:18:35 -0400

    exercise 0 done

--- __workshop/exercise-0.js
+++ __workshop/exercise-0.js
@@ -3,22 +3,22 @@
 // Write a function testNum that takes a number as an argument and returns
 // a Promise that tests if the value is less than or greater than the value 10.
 
-const compareToTen = (num) => {
-  myPromise = new Promise((resolve, reject) => {
-    if (num > 10) {
-      resolve(num + ' is greater than 10, success!');
-    } else {
-      reject(num + ' is less than 10, error!');
-    }
-  });
-  return myPromise;
-};
+const compareToTen = num => {
+  return (
+    new Promise((resolve, reject) => {
+      if(num > 10)
+        resolve(`${num} + is greater than 10, success!`)
+      else
+        reject(`${num} is less than 10, error!`)
+    })
+  )
+}
 
 // Calling the Promise
 compareToTen(15)
-  .then((result) => console.log(result))
-  .catch((error) => console.log(error));
+  .then(result => console.log(result))
+  .catch(error => console.log(error))
 
 compareToTen(8)
-  .then((result) => console.log(result))
-  .catch((error) => console.log(error));
+  .then(result => console.log(result))
+  .catch(error => console.log(error))
